43f9dd21fb4e7223fab1cc5b64686ac88994b608
company <company@qq.com>
2024-06-27 18:58:19 +0800

    update

--- src/random.js
+++ src/random.js
@@ -3,7 +3,7 @@ import vert from '../assets/glsl/common.vert';
 import frag from '../assets/glsl/generative_random.frag';
 import Base from './Base';
 
-export default class CNoise extends Base{
+export default class Random extends Base{
     constructor(regl){
         super(regl,vert,frag)
     }
